3a4994fb095724864533704400928d55410b30f6
maks <maks7458@proton.me>
2026-09-14 21:41:52 +0200

    Add input system

--- src/main.zig
+++ src/main.zig
@@ -7,6 +7,7 @@ const Buffer = utils.Buffer;
 const Window = sdl.video.Window;
 const InitFlags = sdl.InitFlags;
 const FramerateCapper = sdl.extras.FramerateCapper;
+const Input = @import("input.zig").Input;
 
 pub const App = struct {
     const Self = @This();
@@ -17,9 +18,12 @@ pub const App = struct {
     window: Window,
     fps_cap: FramerateCapper(f32),
     dt: f32 = 0,
+    thread_states: []bool,
 
     render_buffer: Buffer,
     display_buffer: Buffer,
+
+    input: Input,
 
     pub fn init(allocator: Allocator) !Self {
         const init_flags = InitFlags{ .video = true };
@@ -38,19 +42,27 @@ pub const App = struct {
 
         const fps_cap = FramerateCapper(f32){ .mode = .{ .limited = 60 } };
 
+        const core_count = try std.Thread.getCpuCount();
+        const thread_states = try allocator.alloc(bool, core_count);
+
         const render_buffer = try Buffer.init(allocator, 100, 100);
         render_buffer.fill(.{ 0, 0, 0, 255 });
 
         const display_buffer = Buffer.from(win_width, win_height, pixels);
         display_buffer.fill(.{ 0, 0, 0, 255 });
 
+        const input = try Input.init(allocator);
+
         return .{
             .init_flags = init_flags,
             .window = window,
             .fps_cap = fps_cap,
+            .thread_states = thread_states,
 
             .render_buffer = render_buffer,
             .display_buffer = display_buffer,
+
+            .input = input,
         };
     }
     pub fn deinit(self: *Self, allocator: Allocator) void {
@@ -58,10 +70,45 @@ pub const App = struct {
             std.debug.print("cant disable relative mode !!!", .{});
 
         self.window.deinit();
+        allocator.free(self.thread_states);
+
         self.render_buffer.deinit(allocator);
+
+        self.input.deinit(allocator);
 
         sdl.quit(self.init_flags);
         sdl.shutdown();
+    }
+    pub fn start(self: *Self) !void {
+        self.gameLoop();
+    }
+    fn gameLoop(self: *Self) void {
+        while (self.running) {
+            self.dt = self.fps_cap.delay();
+
+            std.debug.print("FPS: {}\n", .{1 / self.dt});
+
+            self.handleEvents() catch
+                std.debug.print("cant handle events!!!!!!!\n", .{});
+
+            if (self.input.keyDown(.escape))
+                self.running = false;
+
+            self.input.endStep();
+
+            self.window.updateSurface() catch {};
+        }
+    }
+    fn handleEvents(self: *Self) !void {
+        while (sdl.events.poll()) |event| {
+            switch (event) {
+                .quit => self.running = false,
+                .terminating => self.running = false,
+                else => {},
+            }
+
+            self.input.step(event);
+        }
     }
 };
 
@@ -76,4 +123,6 @@ pub fn main() !void {
         app.deinit(gpa);
         _ = allocator.deinit();
     }
+
+    try app.start();
 }
